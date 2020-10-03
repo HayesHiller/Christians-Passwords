a0098013b69c35a24d0b3bc47c00699eb196c880
HayesHiller <hayeshiller@hotmail.com>
2020-10-03 14:45:39 -0600

    able tp generate true and false

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumbers = numbersEl.checked;
+    const hasSymbols = symbolsEl.checked;
+
+   resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumbers, hasSymbols, length);
+}) 
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatePassword = '';
+
+    const typesCount = lower + upper + number + symbol; 
+
+    console.log('typesCount: ', typesCount);
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+    item => Object.values(item)[0];
+
+    console.log('typesArr; ', typesArr);
+}
+
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '~`!@#$%^&*()_-+=|{}[]:;?.<>,';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomNumber());
 
 
 
@@ -13,4 +69,3 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
