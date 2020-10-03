7f1483a3ee91e4c2e60534b65e557511abc77176
HayesHiller <hayeshiller@hotmail.com>
2020-10-02 18:21:06 -0600

    pushing my files

---
+++ script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+
+
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
